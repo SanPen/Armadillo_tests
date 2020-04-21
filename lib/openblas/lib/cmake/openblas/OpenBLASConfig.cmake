5cf28a0ea1359f7c9c16baa49af128177eab2311
santi <santiago.penate.vera@gmail.com>
2020-04-21 18:46:42 +0200

    Added libraries

---
+++ lib/openblas/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.20")
+SET(OpenBLAS_INCLUDE_DIRS /home/santi/Documentos/Bitbucket/gridcal_c/libs/lib_linux64/include)
+SET(OpenBLAS_LIBRARIES /home/santi/Documentos/Bitbucket/gridcal_c/libs/lib_linux64/lib/libopenblas.so)
